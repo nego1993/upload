69fb0fe8825dd0381a01ddc3bdefd6c43b1788dc
Julio Cesar <juliocs78383@gmail.com>
2022-04-18 22:40:21 -0300

    first commit

---
+++ enviar-arquivo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Upload</title>
+</head>
+<body>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		<label for="arquivo">Arquivo</label>
+		<input type="file" name="arquivo" id="arquivo">
+		<button>Enviar</button>
+		
+	</form>
+</body>
+</html>
